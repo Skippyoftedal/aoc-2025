e506c374760aeb89cc9e2fca7527877b259750ed
tso <oftedal03@gmai.com>
2025-12-04 22:15:49 +0100

    feat: add day_x

---
+++ src/days/day_x.cpp
@@ -0,0 +1,26 @@
+#include "DayTemplate.h"
+#include <iostream>
+#include <ranges>
+#include <regex>
+#include <string>
+#include <stack>
+#include <vector>
+#include "../utility/visual.h"
+
+using std::endl, std::pair, std::cout, std::string, std::vector, std::stack, std::format, std::println;
+
+
+
+class Day_X final : public DayTemplate {
+public:
+    std::string part_1(const std::vector<std::string> &lines) override {
+
+        return std::format("{}","" );
+    }
+
+
+    std::string part_2(const std::vector<std::string> &lines) override {
+
+        return std::format("{}", "");
+    }
+};
